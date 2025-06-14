811e9448367ff6eb7dc5e538ace54c4c5214db51
mohammed sebti <mohammed.sebti@agqlabs.com>
2025-06-14 09:03:04 +0200

    first commit324f1

--- noaman_manazil/settings.py
+++ noaman_manazil/settings.py
@@ -107,7 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+
+LANGUAGE_CODE = 'es-ES'
 
 TIME_ZONE = 'UTC'
 
